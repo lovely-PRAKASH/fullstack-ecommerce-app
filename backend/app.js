a405e0f1a0839cb6b3a19967603d8dfdd3996e7e
prakash-2002 <prakashslm27@gmail.com>
2024-09-22 19:05:09 +0530

    backend main file

---
+++ backend/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const path = require("path");
+const cors=require('cors')
+const connectDatabase = require("./config/connectDatabase");
+dotenv.config({ path: path.join(__dirname, "config", "config.env") });
+
+const products = require("./routes/Product");
+const orders = require("./routes/orders");
+
+app.use(express.json());
+app.use(cors());
+connectDatabase();
+app.use("/api/v1/", products);
+app.use("/api/v1/", orders);
+app.listen(process.env.PORT, () => {
+  console.log(
+    `server is listining at port ${process.env.PORT} in ${process.env.NODE_ENV}`
+  );
+});
