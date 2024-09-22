c0eef09d2ce1543f56d7bf110f75a1c9c88ad2bd
prakash-2002 <prakashslm27@gmail.com>
2024-09-23 00:38:50 +0530

    navigate, search fn added

--- e-commerce-dashboard/src/Components/Header/SearchBox/SearchBox.jsx
+++ e-commerce-dashboard/src/Components/Header/SearchBox/SearchBox.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 
 const SearchBox = () => {
+  const [keyWord, setKeyWord]=useState('');
+  const navigate=useNavigate();
+
+  const searchHandler=()=>{
+    navigate('/search?keyword='+keyWord)
+  }
   return (
     <>
       <div className="headerSearch ml-3 mr-3">
-        <input type="text" placeholder="Search for products..." />
-        <Button className="searchIcon">
+        <input type="text" placeholder="Search for products..." onChange={(e)=>setKeyWord(e.target.value)}/>
+        <Button className="searchIcon" onClick={searchHandler}>
 {/*           search icon */}
           <IoIosSearch />
         </Button>
