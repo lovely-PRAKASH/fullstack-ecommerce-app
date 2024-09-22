b924dfe889abc486e0937fd4c868ef2f53f571c3
prakash-2002 <prakashslm27@gmail.com>
2024-09-23 00:36:29 +0530

    added db port

--- backend/config/connectDatabase.js
+++ backend/config/connectDatabase.js
@@ -1,8 +1,13 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URL).then((con)=>{
-        console.log('MongoDB connected to host: '+con.connection.host)
-    })
-}
-module.exports=connectDatabase;
+const connectDatabase = () => {
+  mongoose.connect(process.env.DB_URL).then((con) => {
+    console.log(
+      "MongoDB connected to host: " +
+        con.connection.host +
+        " on port: " +
+        con.connection.port
+    );
+  });
+};
+module.exports = connectDatabase;
