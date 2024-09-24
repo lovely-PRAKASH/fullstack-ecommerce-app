c72606cfc5be583b3fcb5c05a0d3fcf5fcd655e0
prakash-2002 <prakashslm27@gmail.com>
2024-09-25 00:20:25 +0530

    register model created

---
+++ backend/models/registerModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const registerSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,  // To prevent duplicate emails
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const registerModel = mongoose.model("Register", registerSchema);
+
+module.exports = registerModel;
