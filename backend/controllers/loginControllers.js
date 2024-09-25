bd99569cb43ff633cfc45846bed6c668fccfef60
prakash-2002 <prakashslm27@gmail.com>
2024-09-25 16:45:10 +0530

    login control added

---
+++ backend/controllers/loginControllers.js
@@ -0,0 +1,28 @@
+const registerModel = require('../models/registerModel');
+
+exports.createLogin = async (req, res, next) => {
+  try {
+    console.log(req.body, 'data');
+    const { email, password } = req.body;
+
+    // Find user by email
+    const user = await registerModel.findOne({ email: email });
+
+    if (!user) {
+      // User not found
+      return res.status(404).json({ success: false, message: 'No record exists' });
+    }
+
+    // Check if the password matches
+    if (user.password === password) {
+      return res.status(200).json({ success: true, message: 'Login success' });
+    } else {
+      // Password mismatch
+      return res.status(401).json({ success: false, message: 'Incorrect password or email address' });
+    }
+
+  } catch (err) {
+    // Handle errors
+    return res.status(500).json({ success: false, error: err.message });
+  }
+};
