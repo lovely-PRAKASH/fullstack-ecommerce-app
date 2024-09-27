79e2d0d33ffd36c159e3f41e758a687d0691680b
prakash-2002 <prakashslm27@gmail.com>
2024-09-27 20:52:10 +0530

    error handled

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ const products = require("./routes/Product");
 const orders = require("./routes/orders");
 const register = require("./routes/registers");
 const login = require("./routes/login");
-const payment=require("./routes/payment")
+const errorMiddleWare=require('./utils/middlewares/error')
+// const payment=require("./routes/payment")
 
 app.use(express.json());
 app.use(cors());
@@ -19,8 +20,8 @@ app.use("/api/v1/", products);
 app.use("/api/v1/", orders);
 app.use("/api/v1/", register);
 app.use("/api/v1/", login);
-app.use('/api/v1/', payment)
-
+// app.use('/api/v1/', payment)
+app.use(errorMiddleWare)
 app.listen(process.env.PORT, () => {
   console.log(
     `server is listining at port ${process.env.PORT} in ${process.env.NODE_ENV}`
